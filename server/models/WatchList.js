28b1478822a20eb60ca68ec51fc35095835dfabc
Rajni Dua <rajni.dua14@gmail.com>
2021-12-07 23:23:10 +1030

    added watchList model

---
+++ server/models/WatchList.js
@@ -0,0 +1,36 @@
+const { Schema } = require("mongoose");
+
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedWatchList` array in User.js
+const watchListSchema = new Schema({
+  // saved watchList id from OMDB API
+  imdbId: {
+    type: String,
+    required: true,
+  },
+  poster: {
+    type: String,
+  },
+
+  title: {
+    type: String,
+    required: false,
+  },
+  resultType: {
+    type: String,
+    required: false,
+  },
+  releasedYear: {
+    type: Number,
+    required: false,
+  },
+  plotType: {
+    type: string,
+    required: false,
+  },
+  returnType: {
+    type: string,
+    required: false,
+  },
+});
+
+module.exports = watchListSchema;
