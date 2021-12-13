92622bb70f56a564cbec2156ec7551fe3a36be75
Rajni Dua <rajni.dua14@gmail.com>
2021-12-13 15:45:19 +1030

    updated the mutations at the client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,63 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      token
+      user {
+        username
+        _id
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_WATCHLIST = gql`
+  mutation saveWatchList($myWatchList: SavedWatchListInput!) {
+    saveWatchList($myWatchList: myWatchList) {
+      username
+      savedWatchList {
+         _id
+    imdbId
+    poster
+    title
+    resultType
+    releasedYear
+    plotType
+    returnType
+      }
+    }
+  }
+`;
+
+export const REMOVE_WATCHLIST = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      watchListCount
+      savedWatchList {
+        imdbId
+        poster
+        title
+        resultType
+        releasedYear
+        plotType
+        returnType
+      }
+    }
+  }
+`;
