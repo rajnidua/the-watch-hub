ab2a5b25ca018a0615f2a70b1f69514ce40c207a
Rajni Dua <rajni.dua14@gmail.com>
2021-12-14 13:34:21 +1030

    added styling to the show list component

--- client/src/components/ShowList.js
+++ client/src/components/ShowList.js
@@ -1,20 +1,26 @@
 import React from "react";
+import "../styles/showList.css";
 
 const ShowList = (props) => {
   console.log("list from search bar&&&&&&&& ", props);
 
   return (
-    <div>
-      <div>it is loading</div>
+    <div className="SearchBarData-container">
       <div>{props.searchBarData.length}</div>
-      <div>
-        {props.searchBarData.map((movie) => (
-          <div key={movie.imdbID}>
-            <p>{movie.imdbID}</p>
-            <p>{movie.title}</p>
+
+      {props.searchBarData.map((movie) => (
+        <div key={movie.imdbID} className="data-row">
+          <div className="crypto-name">
+            <div className="img-section">
+              <img src={movie.Poster} alt="image" />
+            </div>
+            <div className="name-section">
+              <div className="title">{movie.Title}</div>
+              <div className="released-year">{movie.Year}</div>
+            </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
@@ -25,10 +31,10 @@ export default ShowList;
             resultType: movie.Type, releasedYear: movie.Year,  plotType:
             movie.Plot,  */
 /*
-      <div className="crypto-container">
+      <div className="SearchBarData-container">
         <p>it is result </p>
         {Object.keys(props.searchBarResult).Search.map((dataRow) => (
-          <div className="crypto-row" key={dataRow.imdbID}>
+          <div className="data-row" key={dataRow.imdbID}>
             <div className="crypto-name">
               <div className="img-section">
                 <img src={dataRow.Poster} alt="image" />
